362182a5537873aeac6c31f1927372e3d1c5041a
penguinio-maker <penguinwebwork@gmail.com>
2026-05-26 22:14:41 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
+import Phaser from 'phaser';
+import { ArenaScene } from './scenes/ArenaScene';
+import './styles.css';
 
+new Phaser.Game({
+  type: Phaser.AUTO,
+  parent: 'game',
+  width: 1280,
+  height: 720,
+  backgroundColor: '#172136',
+  antialias: true,
+  physics: {
+    default: 'arcade',
+    arcade: { debug: false },
+  },
+  scene: [ArenaScene],
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+});
